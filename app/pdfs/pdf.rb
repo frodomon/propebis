b4778fa276a6afa219670bcf5daeefeaf107c866
Alfredo Vasquez <vasquez.alf@gmail.com>
2017-02-08 11:32:09 -0500

    correccion v3 a4

--- app/pdfs/pdf.rb
+++ app/pdfs/pdf.rb
@@ -61,7 +61,7 @@ class Pdf < Prawn::Document
     text_box "#{@rg.final_point }", at: [80, cursor], :style => :bold
     text " "
     if a4
-      move_down 14
+      move_down 18
     else
       move_down 24
     end
