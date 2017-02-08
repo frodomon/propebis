0a31a9228ae66f480684b63be16cd240068987bb
Alfredo Vasquez <vasquez.alf@gmail.com>
2017-02-08 10:56:45 -0500

    menos espacio guia de remision

--- app/pdfs/pdf.rb
+++ app/pdfs/pdf.rb
@@ -44,7 +44,7 @@ class Pdf < Prawn::Document
   	if a4
       move_down 86
     else
-      move_down 56
+      move_down 50
     end
   	text_box "#{@rg.date.strftime("%d - %b - %Y") }", at: [80,cursor], :style => :bold
 		text " "
@@ -54,13 +54,13 @@ class Pdf < Prawn::Document
 		move_down 3
 		text_box "#{@client.ruc}", at: [80, cursor], :style => :bold
 		text " "
-		move_down 13
+		move_down 5
     text_box "#{@rg.initial_point }", at: [80, cursor], :style => :bold
     text " "
     move_down 4
     text_box "#{@rg.final_point }", at: [80, cursor], :style => :bold
     text " "
-    move_down 20
+    move_down 10
     text_box "#{@vehicle.trademark} - #{@vehicle.plate}", at: [80, cursor], :style => :bold
     text " "
     move_down 14
