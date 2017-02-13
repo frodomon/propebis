a5bb3ef603a9afd969fb9077b569f7b2e5fdbead
Alfredo Vasquez <vasquez.alf@gmail.com>
2017-02-13 17:15:09 -0500

    quitar marca obligatoria en producto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
   validates_presence_of :name, message: "Debe ingresar un nombre del producto"
   validates_presence_of :unit_of_measurement, message: "Debe ingresar la unidad de medida del producto"
-  validates_presence_of :trademark, message: "Debe ingresar la marca del producto"
   validates_presence_of :category_id, message: "Debe elegir una categoría para el producto"
   validates_presence_of :sku, message: "Debe ingresar el código SKU para el producto"
 
